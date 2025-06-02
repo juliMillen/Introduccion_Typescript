7ad9aa88fbea99f547fb96cfef4df9658943852d
Julian Millen <julimillen@gmail.com>
2025-06-02 19:19:49 -0300

    comentarios,tipos y variables

---
+++ hello_word.js
@@ -0,0 +1,13 @@
+// Comentarios
+/*
+Otro tipo de comentarios
+*/
+console.log("Hola, Typescript!");
+//variables
+var myString = "Esto es mi cadena de texto";
+//myString = 5 da error(concepto de tipado fuerte myString es de tipo string)  
+console.log(myString);
+var myString2 = "Otra cadena de texto";
+myString2 = "Cambiando el valor de la cadena de texto";
+console.log(myString2);
+console.log(typeof myString2);
--- hello_word.ts
+++ hello_word.ts
@@ -1,1 +1,18 @@
-console.log("Hola, Typescript!");
+
+// Comentarios
+
+/*
+Otro tipo de comentarios
+*/
+
+console.log("Hola, Typescript!")
+
+//variables
+var myString = "Esto es mi cadena de texto"
+//myString = 5 da error(concepto de tipado fuerte myString es de tipo string)  
+console.log(myString)
+
+let myString2 = "Otra cadena de texto"
+myString2 = "Cambiando el valor de la cadena de texto"
+console.log(myString2)
+console.log(typeof myString2)
