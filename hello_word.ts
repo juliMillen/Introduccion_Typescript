2e2ab3940cad772c0e78cf337044b004fc7d0f34
Julian Millen <julimillen@gmail.com>
2025-06-02 19:33:47 -0300

    sintaxis y comprobacion

--- hello_word.ts
+++ hello_word.ts
@@ -16,3 +16,32 @@ let myString2 = "Otra cadena de texto"
 myString2 = "Cambiando el valor de la cadena de texto"
 console.log(myString2)
 console.log(typeof myString2)
+
+let myNumber = 7
+myNumber = myNumber + 3
+console.log(myNumber)
+console.log(typeof myNumber)
+console.log(myNumber - 1)
+console.log(myNumber)
+
+//------------------------
+console.log(myString + " " + myNumber)
+
+let myNumber2 = 8.5
+console.log(myNumber2)
+console.log(typeof myNumber2)
+
+console.log(myNumber + myNumber2)
+
+let myBool: boolean = false
+myBool = true
+console.log(myBool)
+console.log(typeof myBool)
+
+//controles de flujo
+
+if(myNumber == 10){
+    console.log('El valor es 10')
+}else{
+    console.log('El valor no es 10')
+}
